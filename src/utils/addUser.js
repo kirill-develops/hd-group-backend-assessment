be9d847bb02433f54236224377f88c69f1908edf
Kirill Tchentsov <kirill.develops@gmail.com>
2022-09-16 01:08:16 -0400

    created addUser helper function

---
+++ src/utils/addUser.js
@@ -0,0 +1,42 @@
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+} from 'amazon-cognito-identity-js';
+
+const poolData = {
+  UserPoolId: process.env.GATSBY_AWS_USER_POOL_ID,
+  ClientId: process.env.GATSBY_AWS_CLIENT_ID,
+};
+
+const userPool = new CognitoUserPool(poolData);
+const attributeList = [];
+
+export function addUser({ email, photo, password }) {
+
+  const dataEmail = {
+    Name: 'email',
+    Value: email,
+  };
+  const dataPhoto = {
+    Name: 'picture',
+    Value: 'temp' // ! change to s3 address to file
+  };
+
+  const attributeEmail = new CognitoUserAttribute(dataEmail);
+  const attributePhoto = new CognitoUserAttribute(dataPhoto);
+  attributeList.push(attributeEmail);
+  attributeList.push(attributePhoto);
+
+  userPool.signUp(email, password, attributeList, null, function (
+    err,
+    result
+  ) {
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+      // ! delete photo from S3 bucket on fail
+      return;
+    }
+    const cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+  });
+}
