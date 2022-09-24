b788d893321ab1f85fc19807baab0f2d08550daa
Kirill Tchentsov <kirill.develops@gmail.com>
2022-09-24 18:41:26 -0400

    bug: corrected COGNITO_ID value & called initilizeCognitoUser helper function

--- src/utils/authenticateUser.js
+++ src/utils/authenticateUser.js
@@ -1,47 +1,40 @@
 // for verifying email and password
 import * as AWS from 'aws-sdk/global';
-import { userPool } from './addUser';
-import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
+import { AuthenticationDetails } from 'amazon-cognito-identity-js';
+import { navigate } from 'gatsby';
+import { initilizeCognitoUser } from './initializeCognitoUser';
 
 export function authenticateUser({ email, password }) {
+
   const authenticationData = {
     Username: email,
     Password: password,
   };
 
-  const userData = {
-    Username: email,
-    Pool: userPool,
-  };
+  const cognitoUser = initilizeCognitoUser(email);
 
   const authenticationDetails = new AuthenticationDetails(authenticationData);
 
-  const cognitoUser = new CognitoUser(userData);
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
       const accessToken = result.getAccessToken().getJwtToken();
-      const COGNITO_ID = `cognito-idp.${process.env.GATSBY_AWS_S3_REGION}.com/${process.env.GATSBY_AWS_USER_POOL_ID}`
+      const COGNITO_ID = `cognito-idp.${process.env.GATSBY_AWS_S3_REGION}.amazonaws.com/${process.env.GATSBY_AWS_USER_POOL_ID}`
 
-      //POTENTIAL: Region needs to be set if not already set previously elsewhere.
       AWS.config.region = process.env.GATSBY_AWS_S3_REGION;
 
       AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: process.env.GATSBY_AWS_IDENTITY_POOL_ID, // your identity pool id here
+        IdentityPoolId: process.env.GATSBY_AWS_IDENTITY_POOL_ID,
         Logins: {
-          // Change the key below according to the specific region your user pool is in.
           [COGNITO_ID]: result
             .getIdToken()
             .getJwtToken(),
         },
       });
 
-      //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
       AWS.config.credentials.refresh(error => {
         if (error) {
           console.error(error);
         } else {
-          // Instantiate aws sdk service objects now that the credentials have been updated.
-          // example: var s3 = new AWS.S3();
           console.log('Successfully logged!');
         }
       });
