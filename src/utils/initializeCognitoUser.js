fd3795b24335868f2cb497c4b5362070e8d3dce0
Kirill Tchentsov <kirill.develops@gmail.com>
2022-09-24 22:51:49 -0400

    initializeCogniteUser: refactored to refresh session if exsists, create session if credentials, and redirect if no credentials provided provided

--- src/utils/initializeCognitoUser.js
+++ src/utils/initializeCognitoUser.js
@@ -1,12 +1,47 @@
 import { CognitoUser } from "amazon-cognito-identity-js";
 import { userPool } from "./addUser";
+import * as AWS from 'aws-sdk/global';
+import { navigate } from "gatsby";
+
 
 export function initilizeCognitoUser(email) {
+  let cognitoUser = userPool.getCurrentUser();
+
   const userData = {
     Username: email,
     Pool: userPool,
   };
 
-  const cognitoUser = new CognitoUser(userData);
+  if (cognitoUser != null) {
+    cognitoUser.getSession((err, result) => {
+      const COGNITO_ID = `cognito-idp.${process.env.GATSBY_AWS_S3_REGION}.amazonaws.com/${process.env.GATSBY_AWS_USER_POOL_ID}`
+
+      if (result) {
+        console.log('You are now logged in.');
+
+        // Add the User's Id Token to the Cognito credentials login map.
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: process.env.GATSBY_AWS_IDENTITY_POOL_ID,
+          Logins: {
+            [COGNITO_ID]: result
+              .getIdToken()
+              .getJwtToken(),
+          }
+        });
+      } else if (err) {
+        console.log(err);
+      }
+    });
+  } else if (email) {
+    cognitoUser = new CognitoUser(userData);
+  } else {
+    navigate("../signIn");
+    alert("You are not logged in")
+  }
+
+  initilizeCognitoUser.signOut = () => {
+    cognitoUser.signOut();
+  };
+
   return cognitoUser;
 }
